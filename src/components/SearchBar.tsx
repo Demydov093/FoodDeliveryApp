ea897939fabecd37af9aa10001a8c6cfac7ef1cb
DmytriyDemydov <dmytriy.demydov@gmail.com>
2025-11-09 10:38:38 +0100

    added Header and SearchBar to HomeScreen

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+import FontAwesome from '@react-native-vector-icons/fontawesome';
+
+const SearchBar = ({
+  value,
+  onChange,
+}: {
+  value?: string;
+  onChange?: (v: string) => void;
+}) => {
+  return (
+    <View className="bg-white rounded-full h-12 flex-row items-center px-4 shadow">
+      <FontAwesome name="search" size={16} color="#9CA3AF" />
+      <TextInput
+        value={value}
+        onChangeText={onChange}
+        placeholder="Search"
+        placeholderTextColor={'#9CA3AF'}
+        className="ml-3 flex-1 text-base"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { StatusBar, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 
 const HomeScreen = () => {
+  const [query, setQuery] = useState('');
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView edges={['top']} className="flex-1 bg-green-700">
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor="#15803d"
+        translucent={false}
+      />
+      <View className="bg-green-700">
+        <Header />
+        <View className="px-4 pb-4">
+          <SearchBar value={query} onChange={setQuery} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
